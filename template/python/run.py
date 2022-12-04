813a9a08bb3d185f54d63fe3af695f185470291a
Simon King <Simon.King@ncipher.com>
2022-12-04 12:45:34 +0000

    add template day* code

---
+++ template/python/run.py
@@ -0,0 +1,34 @@
+
+import os
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+INPUT = os.path.join(HERE, "../input.txt")
+
+
+def part_1():
+    print("Part 1")
+
+    with open(INPUT, 'r') as it:
+        data = it.readlines()
+
+
+
+
+def part_2():
+    print("Part 2")
+
+    with open(INPUT, 'r') as it:
+        data = it.readlines()
+
+
+
+
+def main():
+    print("Day 1")
+
+    part_1()
+    part_2()
+
+
+if __name__ == "__main__":
+    main()
